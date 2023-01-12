4f89a2506653b1fc7c4294d73706a6e90a5dc244
Jacek Wiśniewski <119750860+roller0201@users.noreply.github.com>
2023-01-12 18:46:20 +0200

    12.01.23

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const switchButton = document.querySelector("header button");
 
 switchButton.addEventListener("click", () => {
   document.querySelector("body").style.backgroundColor = "#000";
-  document.querySelector("body").style.backgroundColor = "white";
+  document.querySelector("body").style.backgroundColor = "#fff";
+  document.querySelector("main button").style.backgroundColor = "#000";
+  document.querySelector("main button").style.backgroundColor = "#fff";
 });
