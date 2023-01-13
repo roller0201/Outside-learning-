c7cba0ca76bf803281a54f8024cbed4e36db8379
Jacek Wiśniewski <119750860+roller0201@users.noreply.github.com>
2023-01-13 12:45:54 +0200

    13.01.23

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const switchButton = document.querySelector("header button");
 
 switchButton.addEventListener("click", () => {
-  document.querySelector("body").classList.add("dark");
+  document.querySelector("body").style.backgroundColor = "#000";
 });
